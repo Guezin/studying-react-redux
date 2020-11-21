93e1635588f9dc3ca72bf347c47203834908cbd5
Guezin <leandroguezinjunior@gmail.com>
2020-11-21 18:31:10 -0300

    integrando a lib immer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from 'redux'
+import produce from 'immer'
 
 import { ICartState } from './types'
 
@@ -7,25 +8,34 @@ const INITIAL_STATE: ICartState = {
 }
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  switch(action.type) {
-    case 'ADD_PRODUCT_TO_CART': {
-      const { product } = action.payload;
-
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
+  return produce(state, draft => {
+    switch(action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload;
+        
+          draft.items.push({
             product,
             quantity: 1
-          }
-        ]
+          })
+
+          break
+
+        // return {
+        //   ...state,
+        //   items: [
+        //     ...state.items,
+        //     {
+        //       product,
+        //       quantity: 1
+        //     }
+        //   ]
+        // }
+      }
+      default: {
+        return draft
       }
     }
-    default: {
-      return state
-    }
-  }
+  }) 
 }
 
 export default cart
