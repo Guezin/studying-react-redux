582f86a719222ba791ef25f10c4dad75a25e6848
Guezin <leandroguezinjunior@gmail.com>
2020-11-21 18:34:41 -0300

    criando validacao para carrinho

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -12,13 +12,19 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
     switch(action.type) {
       case 'ADD_PRODUCT_TO_CART': {
         const { product } = action.payload;
+
+        const productInCart = draft.items.findIndex(item => item.product.id === product.id);
         
+        if (productInCart >= 0) {
+          draft.items[productInCart].quantity++
+        } else {
           draft.items.push({
             product,
             quantity: 1
           })
+        }
 
-          break
+        break
 
         // return {
         //   ...state,
